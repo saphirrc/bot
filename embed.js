d41b50880e90eb07e473a8a71d6bd093a7a799c0
saphirrc <59259156+saphirrc@users.noreply.github.com>
2020-04-23 09:29:21 -0400

    Update embed.js

--- embed.js
+++ embed.js
@@ -1,6 +1,6 @@
+const discord = require('discord.js')
 const { Command } = require('discord.js-commando')
-module.exports = class embed extends Command
-{
+module.exports = class embed extends Command {
     constructor(client) {
         super(client, {
             name: 'embed',
@@ -8,62 +8,59 @@ module.exports = class embed extends Command
             aliases: ['embed'],
             group: 'bot',
             description: 'crée un embed',
-            guildOnly: true,
-            
+            guildOnly: true,    
         })
     }
     run(message, arg1){
-       message.delete();
-        const discord = require('discord.js')
+        + message.delete({ timeout: 0, reason: 'It had to be done.' })
         const poll = new discord.MessageEmbed()
         .setTitle('outil de création d\'embed')
-        .setDescription('0⃣ - titre\n\n1⃣ - description\n\n 📨 - terminer')
-        message.channel.send(poll).then((m) =>{
-            m.react('0⃣');
-            m.react('1⃣');
-            m.react('📨');
+        .setDescription('0⃣ - titre\n\n1⃣ - description\n\n📨 - terminer')
+        message.channel.send(poll) .then((m) => {
+            m.react('0⃣')
+            m.react('1⃣')
+            m.react('📨')
             const filter = (reaction, user) => {
-                return ['0⃣', '1⃣', '📨'].includes(reaction.emoji.name) && user.id === message.author.id;
+                return ['0⃣', '1⃣', '📨'].includes(reaction.emoji.name) && user.id === message.author.id;                
             };
             m.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
-                .then(collected => {
-                    const reaction = collected.first();
-                    if (reaction.emoji.name === '0⃣' && reaction.user === message.user){
-                        reaction.remove(message.user)
-                        reaction.emoji.name = '1⃣'
-                        reaction.remove(message.user)
-                        reaction.emoji.name = '📨'
-                        reaction.remove(message.user)
-                        const pol = new discord.MessageEmbed()
-                        .setTitle('quel nom voulez vous que l\'embed aye')
-                        .setDescription('La commande va être annulée dans  60 secondes.')
-                        message.channel.send(pol)
-                        .then((me) =>{
-                            message.channel.createMessageCollector(
-                                msg => msg.author.id === message.author.id,{ max: 1, time: 60000 }
-                            ).on('collect', async msgResponse =>{
-                                if (msgResponse && !msgResponse.deleted) await msgResponse.delete();
-                                if (me && !me.deleted) await me.delete();
-                                m.delete();
-                                console.log(msgResponse.content)
-                                const arg1 = msgResponses
-                                this.run(message, arg1)
-                            }).on('end', (_collected, reason) =>{
-                                if (reason !== 'limit') return message.reply('les 60 secondes sont écoulées !')
-                            });
-                        })
-                    }if(reaction.emoji.name === '1⃣' && reaction.user === message.user){
-                        // à venir
-                    }if(reaction.emoji.name === '📨' && reaction.user === message.user){
-                        const emb = new discord.MessageEmbed()
-                        emb.setTitle(arg1)
-                        message.channel.send(emb)
-                        m.delete();
-                    }
-                })
+            .then(collected => {
+                const reaction = collected.first();
+                if (reaction.emoji.name === '0⃣') {
+                    m.delete();
+                    
+                    poll.setTitle('quel nom voulez vous que l\'embed porte')
+                    poll.setDescription('La commande va être annulée dans  30 secondes.')
+                    message.channel.send(poll).then((me) => {
+                    message.channel.createMessageCollector(
+                        m => m.author.id === message.author.id,{ max: 1, time: 60000 }
+                        ).on('collect', async msgResponse => {
+                            if (msgResponse && !msgResponse.deleted) await msgResponse.delete();
+                            // là tu fais ce que tu veux avec le contenu de ton message
+                            console.log(msgResponse.content)
+                            me.delete();    
+                            m.delete();
+                            const arg1 = msgResponse
+                            this.run(message, arg1)
+                        }).on('end', (_collected, reason) => {
+                            if (reason !== 'limit') return message.reply('les 60 secondes sont écoulées !');
+                        });
+                    })
+                }if(reaction.emoji.name === '📨'){
+                    m.delete();
+                    this.embedc(message, arg1)
+                }if(reaction.emoji.name === '1⃣'){  
+                    
+                }
+            })
             .catch(collected => {
                 message.reply('time out');
-            });
+            }); 
         })
-    } 
+    }
+    embedc(message, arg1){
+        const res = new discord.MessageEmbed()
+        .setTitle(arg1)
+       message.channel.send(res)
+    }
 }
